e67be4569dc1e6089ac66f21c48f60d8ebb77e75
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-02 10:37:41 +0900

    3번째: NavTag 생성

--- src/App.js
+++ src/App.js
@@ -14,12 +14,29 @@ function HeaderTag() {
   );
 }
 
+// header태그 컴포넌트 생성
+function NavTag() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="1.html">html</a>
+        </li>
+        <li>
+          <a href="1.html">css</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
 
 
 function App() {
   return (
     <div className="App">
         <HeaderTag></HeaderTag>
+        <NavTag></NavTag>
     </div>
   );
 }
